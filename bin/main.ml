ae468b0d8a50091a0f34824e32a980eecfebf5b2
Tomás Tavares <tomastavares21122001@gmail.com>
2024-05-02 12:21:19 +0100

    verbose mode

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,19 @@
 open Cmdliner
 
-let main (filename : string) : int =
+let main (filename : string) (verbose : bool) : int =
   match Auxiliary.Js_parser.from_file filename with
   | Ok ast ->
       let norm_program = Normalizer.normalize ast in
-      let graph, store = Mdg.Analyse.program norm_program in 
+      let graph, store = Mdg.Analyse.program verbose norm_program in 
 
-      print_endline "Graph";
+      print_endline "Graph\n-----";
       Mdg.Graph.print graph;
 
-      print_endline "Store";
+      print_endline "Store\n-----";
       Mdg.Store.print store;
 
-
       let js_program = Pp.Js.print norm_program in
+      print_endline "Code\n-----";
       print_endline js_program;
       0
   | Error msg ->
@@ -27,8 +27,12 @@ let input_file : string Term.t =
   let docv = "FILE" in
   Arg.(required & pos 0 (some non_dir_file) None & info [] ~doc ~docv)
 
+let verbose : bool Term.t =
+  let doc = "Enable verbose mode" in
+  Arg.(value & flag & info ["v"; "verbose"] ~doc)
+
 let cli =
-  let cmd = Term.(const main $ input_file) in
+  let cmd = Term.(const main $ input_file $ verbose) in
   let info = Cmd.info "ast_gen" in
   Cmd.v info cmd
 
