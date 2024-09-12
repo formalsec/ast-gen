9e23b143a9ab01f58c2ddeb27e8e82a35a830046
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-12 18:52:05 +0100

    Improve cli abstractions
    Use `Cmdliner.Arg.file` conv to parse valid files. Cmdliners' file
    conv already does a `Sys.file_exists` so we avoid code duplication
    here.
    
    Also define `Mode.t` as an algebraic datatype to reduce string
    allocations and allow for faster instant `is_` mode checks.

--- bin/main.ml
+++ bin/main.ml
@@ -10,24 +10,24 @@ open Auxiliary.Functions
 
 let env_path = Filename.dirname (Filename.dirname Sys.executable_name) ^ "/lib/ast_gen/";;
 
-let setup_output (output_path : string) : (string * string * string) = 
-  let code_dir = output_path  ^ "/code/" in 
-  let graph_dir = output_path ^ "/graph/" in 
+let setup_output (output_path : string) : (string * string * string) =
+  let code_dir = output_path  ^ "/code/" in
+  let graph_dir = output_path ^ "/graph/" in
   let run_dir   = output_path ^ "/run/" in
 
   File_system.clean_dir output_path;
   File_system.create_dir code_dir;
   File_system.create_dir graph_dir;
   File_system.create_dir run_dir;
-  
+
   code_dir, graph_dir, run_dir
 
-let setup_node (mode : string) : string = 
+let setup_node (mode : Mode.t) : string =
   (* !REFACTOR : inbed this functionality into the dune build and install process *)
-  let js_folder    = env_path ^ "js/" in 
-  let node_modules = js_folder ^ "node_modules" in 
-  let package_info = js_folder ^ "package.json" in 
-  let script       = js_folder ^ "generate_cg.js" in 
+  let js_folder    = env_path ^ "js/" in
+  let node_modules = js_folder ^ "node_modules" in
+  let package_info = js_folder ^ "package.json" in
+  let script       = js_folder ^ "generate_cg.js" in
 
   if not (Sys.file_exists script)
     then failwith "[ERROR] Dependency tree genetarion script not found";
@@ -38,63 +38,63 @@ let setup_node (mode : string) : string =
   if Mode.is_multi_file mode && not (Sys.file_exists node_modules)
     then (
       print_endline "installing js dependencies";
-      let result = Sys.command ("npm install --prefix " ^ js_folder) in 
+      let result = Sys.command ("npm install --prefix " ^ js_folder) in
       if result != 0 then failwith "[ERROR] Unable to install js depedencies";
       print_endline "DONE!");
 
   script
 
 
-let main (filename : string) (output_path : string) (config_path : string) (mode : string) (generate_mdg : bool) (no_dot : bool) (verbose : bool) : int =
-  
+let main (filename : string) (output_path : string) (config_path : string) (mode : Mode.t) (generate_mdg : bool) (no_dot : bool) (verbose : bool) : int =
+
   (* SETUP *)
   let script = setup_node mode in
-  let dep_tree = DependencyTree.generate script filename mode in  
-  let code_dir, graph_dir, _ = setup_output output_path in 
+  let dep_tree = DependencyTree.generate script filename mode in
+  let code_dir, graph_dir, _ = setup_output output_path in
 
   (* process dependencies first with the aid of the depedency tree *)
-  let summaries = Summaries.empty () in 
-  let module_graphs = ModuleGraphs.empty () in 
-  List.iter (fun file_path -> 
-    let dir = Filename.dirname file_path ^ "/" in 
-    let filename = File_system.file_name file_path in 
+  let summaries = Summaries.empty () in
+  let module_graphs = ModuleGraphs.empty () in
+  List.iter (fun file_path ->
+    let dir = Filename.dirname file_path ^ "/" in
+    let filename = File_system.file_name file_path in
 
     (* STEP 0 : Generate AST using Flow library *)
-    let ast = Js_parser.from_file file_path in 
+    let ast = Js_parser.from_file file_path in
 
     (* STEP 1 : Normalize AST *)
     let norm_program = Ast.Normalize.program ast file_path in
-    let norm_program = if file_path = dep_tree.main then Program.set_main norm_program else norm_program in 
+    let norm_program = if file_path = dep_tree.main then Program.set_main norm_program else norm_program in
     let js_program = Ast.Pp.Js.print norm_program in
     File_system.write_to_file (code_dir ^ filename) js_program;
 
     (* STEP 2 : Generate MDG for the normalized code *)
     if generate_mdg then (
       let graph, exportedObject, external_calls = Mdg.Analyse.program mode verbose config_path norm_program in
-    
+
       ExternalReferences.iter (fun locs info ->
         let l_call = LocationSet.min_elt locs in
-        
-        (* module information *) 
-        let module_name = dir ^ info._module in 
-        let moduleEO = Summaries.get_opt summaries module_name in 
+
+        (* module information *)
+        let module_name = dir ^ info._module in
+        let moduleEO = Summaries.get_opt summaries module_name in
         option_may (fun moduleEO ->
-          let moduleGraph = ModuleGraphs.get module_graphs module_name in 
-          
+          let moduleGraph = ModuleGraphs.get module_graphs module_name in
+
           (* exported function information *)
-          let func_loc = ExportedObject.get_value_location moduleEO info.properties in 
+          let func_loc = ExportedObject.get_value_location moduleEO info.properties in
           if not (Graph.has_external_function graph func_loc) then (
-            let func_graph = Graph.get_function moduleGraph func_loc in 
+            let func_graph = Graph.get_function moduleGraph func_loc in
             Graph.add_external_func graph func_graph l_call func_loc
           );
-          
+
           Graph.add_call_edge graph l_call func_loc;
         ) moduleEO;
 
       ) external_calls;
-      
+
       (* save current module info*)
-      let alter_name = String.sub file_path 0 (String.length file_path - 3) in 
+      let alter_name = String.sub file_path 0 (String.length file_path - 3) in
       ModuleGraphs.add module_graphs file_path graph;
       ModuleGraphs.add module_graphs alter_name graph;
       Summaries.add summaries file_path exportedObject;
@@ -105,9 +105,9 @@ let main (filename : string) (output_path : string) (config_path : string) (mode
 
   (* output *)
   if generate_mdg then (
-    let main = DependencyTree.get_main dep_tree in 
-    let graph = ModuleGraphs.get module_graphs main in 
-    if not no_dot then 
+    let main = DependencyTree.get_main dep_tree in
+    let graph = ModuleGraphs.get module_graphs main in
+    if not no_dot then
       Mdg.Pp.Dot.output graph_dir graph;
     Mdg.Pp.CSV.output graph_dir graph;
   );
@@ -118,18 +118,18 @@ let main (filename : string) (output_path : string) (config_path : string) (mode
 let input_file : string Term.t =
   let doc = "Path to JavaScript file (.js) or directory containing JavaScript files for analysis." in
   let docv = "FILE_OR_DIR" in
-  let is_dir_or_file (param : string) : string =  
-    if Sys.file_exists param 
-      then param
-      else failwith ("[ERROR] Invalid input file : " ^ param) 
-  in 
+  Arg.(required & pos 0 (some file) None & info [] ~doc ~docv)
 
-  Term.(const is_dir_or_file $  Arg.(required & pos 0 (some string) None & info [] ~doc ~docv))
-
-
-let mode : string Term.t =
+let mode : Mode.t Term.t =
+  let mode_enum =
+    Arg.enum
+      [ ("basic", Mode.Basic)
+      ; ("single_file", Mode.Single_file)
+      ; ("multi_file", Mode.Multi_file);
+      ]
+  in
   let doc = "Analysis mode.\n\t 1) basic: attacker controlls all parameters from all functions \n\t 2) single_file: the attacker controlls the functions that were exported by the input file \n\t 3) multi_file: the attacker controlls the functions that were exported in the \"main\" file" in
-  Term.(const Mode.is_valid $ Arg.(value & opt string Mode.default & info ["m"; "mode"] ~doc))
+  Arg.(value & opt mode_enum Mode.single_file & info ["m"; "mode"] ~doc)
 
 let mdg : bool Term.t =
   let doc = "Generates Multiversion Dependency Graph." in
@@ -141,12 +141,12 @@ let no_dot : bool Term.t =
 
 let output_path : string Term.t =
   let doc = "Path to store all output files." in
-  let default_path = "graphjs-results" in 
+  let default_path = "graphjs-results" in
   Arg.(value & opt string default_path & info ["o"; "output"] ~doc)
 
 let config_path : string Term.t =
   let doc = "Path to configuration file." in
-  let default_path = env_path ^ "config.json" in 
+  let default_path = env_path ^ "config.json" in
   Arg.(value & opt non_dir_file default_path & info ["c"; "config"] ~doc)
 
 let verbose : bool Term.t =
--- lib/setup/dependencyTree.ml
+++ lib/setup/dependencyTree.ml
@@ -5,72 +5,74 @@ type t = {
   structure : Yojson.Basic.t;
 }
 
-let generate_dt (script : string ) (filename : string) : string = 
+let generate_dt (script : string ) (filename : string) : string =
   "node " ^ script ^ " " ^ filename
 
-let single_file_dt (filename : string) : string = 
+let single_file_dt (filename : string) : string =
   "{ \"" ^ filename ^ "\" : {} }"
 
-let rec generate (script : string) (filename : string) (mode : string) : t =
+let rec generate (script : string) (filename : string) (mode : Mode.t) : t =
   let main_file = get_main_file filename mode in
-  let output = if Mode.is_multi_file mode 
-    then File_system.run_command (generate_dt script main_file) 
+  let output = if Mode.is_multi_file mode
+    then File_system.run_command (generate_dt script main_file)
     else single_file_dt main_file
   in
 
   { main      = main_file;
     structure = Yojson.Basic.from_string output }
-    
-and get_main_file (filename : string) (mode : string) : string =
-  let filename = File_system.real_path filename in 
-  match mode with 
+
+and get_main_file (filename : string) (mode : Mode.t) : string =
+  let filename = File_system.real_path filename in
+  match mode with
     (* in single file analysis, the filename should already be the main file*)
-    | mode when Mode.is_basic mode || Mode.is_single_file mode -> 
-      if not (Sys.is_directory filename) 
-        then filename 
-        else failwith "[ERROR] A folder was provided for single file analysis" 
-    
-    (* in multifile analysis, the filename can be the main file or a module 
+    | mode when Mode.is_basic mode || Mode.is_single_file mode ->
+      if not (Sys.is_directory filename)
+        then filename
+        else failwith "[ERROR] A folder was provided for single file analysis"
+
+    (* in multifile analysis, the filename can be the main file or a module
        directory if it is a directory we need to find the main file of the module *)
-    | mode when Mode.is_multi_file mode -> 
-      if not (Sys.is_directory filename) 
+    | mode when Mode.is_multi_file mode ->
+      if not (Sys.is_directory filename)
         then filename
         else find_module_main filename
-    | _ -> failwith "[ERROR] No implementation for mode " ^ mode
+    | _ ->
+      Format.kasprintf failwith "[ERROR] No implementation for mode %a" Mode.pp
+        mode
 
-and find_module_main (module_path : string) : string = 
+and find_module_main (module_path : string) : string =
   (* https://docs.npmjs.com/cli/v10/configuring-npm/package-json#main *)
-  let main_file : (string option) ref  = ref None in 
-  let module_path = ref module_path in 
-  
+  let main_file : (string option) ref  = ref None in
+  let module_path = ref module_path in
+
   (* check package.json *)
-  let package_path = Filename.concat !module_path "package.json" in 
+  let package_path = Filename.concat !module_path "package.json" in
   if Sys.file_exists package_path then (
     let package_info = Yojson.Basic.from_file package_path in
     match Yojson.Basic.Util.member "main" package_info with
-      | `String main -> 
-        let main_path = Filename.concat !module_path main in 
-        if not (Sys.file_exists main_path) 
+      | `String main ->
+        let main_path = Filename.concat !module_path main in
+        if not (Sys.file_exists main_path)
           then failwith "[ERROR] Main file described in package.json doesn't exist"
-        else if Sys.is_directory main_path 
+        else if Sys.is_directory main_path
           then module_path := main_path
           else main_file := Some main_path
 
-          
+
       | _ -> ()
   );
 
   (* check if index.js exists in the root folder *)
-  let index_path = Filename.concat !module_path "index.js" in 
+  let index_path = Filename.concat !module_path "index.js" in
   if Sys.file_exists index_path && Option.is_none !main_file then (
     main_file := Some index_path
   );
 
-  if Option.is_some !main_file 
+  if Option.is_some !main_file
     then Option.get !main_file
-    else failwith ("[ERROR] Unable to find main file of module : " ^ !module_path) 
+    else failwith ("[ERROR] Unable to find main file of module : " ^ !module_path)
 
-  
+
 
 let get_main (dep_tree : t) : string = dep_tree.main
 
@@ -86,6 +88,3 @@ let bottom_up_visit (dep_tree : t) : string list =
   let visit_order = visit dep_tree.structure []  in
   (* remove duplicated from visit *)
   List.rev (List.fold_left (fun final_order curr -> if List.mem curr final_order then final_order else curr :: final_order)  [] visit_order)
-
-
-  
