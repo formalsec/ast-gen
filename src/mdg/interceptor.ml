02864678ec8a543a1f65b4552aabceb7706b1a8f
andreffnascimento <andreffnascimento@outlook.com>
2025-06-20 10:52:16 +0900

    (mdg) add interceptor for function.bind

--- src/mdg/interceptor.ml
+++ src/mdg/interceptor.ml
@@ -21,7 +21,7 @@ module RequireInterceptor = struct
     | _ -> None
 
   let process_module (cb_build_file : cb_build_file) (state : State.t)
-      (mrel : Fpath.t) : Node.Set.t =
+      (retn_cid : Allocator.cid) (mrel : Fpath.t) : Node.Set.t =
     match Pcontext.file state.pcontext mrel with
     | None ->
       let State.{ mdg; pcontext; jslib; npmlib; _ } = state in
@@ -31,8 +31,8 @@ module RequireInterceptor = struct
     | Some file when file.built ->
       Jslib.exported_object ~mrel:file.file.mrel state.mdg state.jslib
     | Some file ->
-      let l_module = Node.create_module' (Fpath.to_string file.file.mrel) in
-      Mdg.add_node state.mdg l_module;
+      let name = Fpath.to_string file.file.mrel in
+      let l_module = State.add_module_node state retn_cid name in
       let state' = cb_build_file state file.file false (Some l_module) in
       Jslib.exported_object ~mrel:file.file.mrel state'.mdg state.jslib
 
@@ -41,15 +41,55 @@ module RequireInterceptor = struct
       State.t =
     match get_module_path state ls_args with
     | None -> state
-    | Some path ->
+    | Some mrel ->
+      let retn_cid = Allocator.offset (Allocator.cid left) 1 in
       let name = LeftValue.name left in
       let kind = LeftValue.kind left in
-      let ls_exports = process_module cb_build_file state path in
+      let ls_exports = process_module cb_build_file state retn_cid mrel in
       Store.write ~kind state.store name ls_exports;
       state
 end
 
-let initialize (state : State.t) (cbs_builder : cbs_builder) : unit =
+module FunctionBindInterceptor = struct
+  let matcher (_ : Node.t) (ls_args : Node.Set.t list) (prop : Property.t) :
+      bool =
+    Node.Set.for_all Node.is_function (List.hd ls_args)
+    && Property.equal (Static "bind") prop
+
+  let get_this_function (state : State.t) (ls_args : Node.Set.t list) :
+      'm Pcontext.func option =
+    let ls_this = List.hd ls_args in
+    if Node.Set.cardinal ls_this == 1 then
+      Pcontext.func state.pcontext (Node.Set.choose ls_this)
+    else None
+
+  let run (state : State.t) (left : Region.t LeftValue.t) (l_func : Node.t)
+      (ls_args : Node.Set.t list) : State.t =
+    let func = get_this_function state ls_args in
+    Fun.flip Option.iter func (fun { func; store; _ } ->
+        let name = LeftValue.name left in
+        let kind = LeftValue.kind left in
+        let func_name = Node.name l_func in
+        let bind_cid = Allocator.offset (Allocator.cid left) 1 in
+        let l_bind = State.add_function_node state bind_cid func_name in
+        Store.write ~kind state.store name (Node.Set.singleton l_bind);
+        let store' = Store.copy store in
+        Pcontext.declare_func state.pcontext l_bind state.curr_floc func store';
+        Fun.flip List.iteri ls_args (fun idx ls_arg ->
+            Fun.flip Option.iter (List.nth_opt func.params idx) (fun param ->
+                let param_name = Identifier.name param in
+                Store.set store param_name ls_arg ) ) );
+    state
+end
+
+let initialize_require (state : State.t) (cbs_builder : cbs_builder) : unit =
   let l_require = Jslib.find_node state.mdg state.jslib "require" in
   let cb_require = RequireInterceptor.run cbs_builder.build_file in
-  State.set_call_interceptor state l_require cb_require
+  State.set_function_interceptor state l_require cb_require
+
+let initialize_function_props (state : State.t) : unit =
+  FunctionBindInterceptor.(State.set_method_interceptor state matcher run)
+
+let initialize (state : State.t) (cbs_builder : cbs_builder) : unit =
+  initialize_require state cbs_builder;
+  initialize_function_props state
